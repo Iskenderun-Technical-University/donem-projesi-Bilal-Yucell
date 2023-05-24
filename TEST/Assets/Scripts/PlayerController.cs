084bdd3100305cef392facf61d3c9c58604ce3f7
Bilal-Yucell <bilallyucell@gmail.com>
2023-05-24 19:13:28 +0300

    Kamera - Mermi - Kontrol
    Kamera ayarlandı, mermi eklendi, oyuncu hareketi aşağı düşmeyecek şekilde kısıtlandı/düzeltildi.

--- TEST/Assets/Scripts/PlayerController.cs
+++ TEST/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float characterSpeed;
+    float touchXDelta;
+    float newX = 0;
+    public float xSpeed;
+    public float limitX;
 
     void Start()
     {
@@ -13,7 +17,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + characterSpeed * Time.deltaTime);
+        SwipeCheck();
+
+    }
+
+    private void SwipeCheck()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitX, limitX);
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + characterSpeed * Time.deltaTime);
         transform.position = newPosition;
     }
+
 }
