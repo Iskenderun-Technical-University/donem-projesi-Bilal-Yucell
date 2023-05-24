6fb8ff638ed7aa33cd8f73373fa90ab87931f44e
Bilal-Yucell <bilallyucell@gmail.com>
2023-05-24 18:38:24 +0300

    PlayerController
    PlayerController

--- TEST/Assets/Scripts/PlayerController.cs
+++ TEST/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float characterSpeed;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + characterSpeed * Time.deltaTime);
+        transform.position = newPosition;
     }
 }
