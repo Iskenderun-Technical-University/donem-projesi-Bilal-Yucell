b60cb0244206e255e84f49fd978f18905320c8a1
Bilal-Yucell <bilallyucell@gmail.com>
2023-05-25 19:17:15 +0300

    Mermiler toplanabilir hale getirildi
    Mermiler toplanabilir hale getirildi

---
+++ TEST/Assets/Scripts/CollectBullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectBullet : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
