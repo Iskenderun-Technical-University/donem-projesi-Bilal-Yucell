36f966f0739905a0ca80e93d13067ac328c9443d
Bilal-Yucell <bilallyucell@gmail.com>
2023-05-27 00:17:02 +0300

    Engel ve materyalleri eklendi, Skor arayüzde gösterilir hale geldi
    Engel ve materyalleri eklendi, Skor arayüzde gösterilir hale geldi

--- TEST/Assets/Scripts/CollectBullet.cs
+++ TEST/Assets/Scripts/CollectBullet.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectBullet : MonoBehaviour
 {
+    public int score;
+    public TextMeshProUGUI bulletText;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
+            AddBullet();
             Destroy(other.gameObject);
         }
+    }
+
+    public void AddBullet()
+    {
+        score++;
+        bulletText.text = "Score: " + score.ToString();
     }
 }
